1d571d69331faa186294afb20a50283420207e1f
RCFS <rcf.schmitz@gmail.com>
2026-06-03 22:52:59 +0200

    Add files via upload

---
+++ 03-Kapitel 3 - Installation & Setup/3.2 Ghost-CLI  Das offizielle Installationswerkzeug/Kap3-2_abb1.sh
@@ -0,0 +1,1 @@
+npm install -g ghost-cli@latest
---
+++ 03-Kapitel 3 - Installation & Setup/3.2 Ghost-CLI  Das offizielle Installationswerkzeug/Kap3-2_abb3.sh
@@ -0,0 +1,7 @@
+ghost install --db=mysql
+--dbhost=localhost \
+--dbuser=ghost
+--dbpass=secret
+--dbname=ghost_prod
+--url=https://meinblog.de
+--no-prompt
