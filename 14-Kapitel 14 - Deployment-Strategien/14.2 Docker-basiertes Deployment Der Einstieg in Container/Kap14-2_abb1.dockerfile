f1ef490803b37411fd584a8f301cc9ce363f92f3
RCFS <rcf.schmitz@gmail.com>
2026-06-04 10:26:52 +0200

    Add files via upload

---
+++ 14-Kapitel 14 - Deployment-Strategien/14.2 Docker-basiertes Deployment Der Einstieg in Container/Kap14-2_abb1.dockerfile
@@ -0,0 +1,6 @@
+FROM
+ghost:5-alpine
+N
+CoPY./mein-theme/var/1ib/ghost/content/themes/mein-theme
+L°
+USERnode
---
+++ 14-Kapitel 14 - Deployment-Strategien/14.2 Docker-basiertes Deployment Der Einstieg in Container/Kap14-2_abb2.sh
@@ -0,0 +1,8 @@
+bashli
+build -t mein-blog
+docker
+run-d-p
+mein-blog
+docker
+2368:2368
+8-e ur1=http://localhost:2368
